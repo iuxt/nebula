a6ec0506f869f25a7e4d5e2a3c759ccbc9092648
iuxt <iuxt@qq.com>
2025-01-07 17:29:02 +0800

    add sync ssl script

---
+++ nginx/rsync_ssl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rsync -avz /root/nebula/nginx/ssl/ root@zhangfei.babudiu.com:/root/nebula/nginx/ssl/
+rsync -avz /root/nebula/nginx/ssl/ root@liubei.babudiu.com:/root/nebula/nginx/ssl/
+
