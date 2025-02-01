66e3896c136b9b50a8b1154629500104d8421c62
zhanglikun <x@zahui.fan>
2025-02-01 14:43:27 +0800

    规范启动脚本名

--- cloudreve/run.sh
+++ cloudreve/run.sh
@@ -22,4 +22,21 @@ podman run -d \
     --restart=always \
     cloudreve/cloudreve:latest
 
+podman run -d \
+    --name aria2 \
+    --network iuxt \
+    --restart unless-stopped \
+    --log-opt max-size=1m \
+    -e PUID=$UID \
+    -e PGID=$(id -g) \
+    -e UMASK_SET=022 \
+    -e RPC_SECRET=com.012 \
+    -e RPC_PORT=6800 \
+    -e LISTEN_PORT=6888 \
+    -v $PWD/aria2-config:/config \
+    -v $PWD/downloads:/downloads \
+    p3terx/aria2-pro
+
+
+
 ../public/add_config_to_nginx.sh
--- cloudreve/stop.sh
+++ cloudreve/stop.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 cd $(dirname $0)
 
-podman rm -f cloudreve
+podman rm -f cloudreve aria2
 
 rm -f ../nginx/conf.d/"$(basename "$(pwd)")".conf
 ../nginx/reload_nginx.sh
