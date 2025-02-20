f4a2a7d2cfcbb26b5a8385df864bc44b69b5077d
iuxt <iuxt@qq.com>
2025-02-21 00:24:57 +0800

    config file

--- easytier/run.sh
+++ easytier/run.sh
@@ -8,5 +8,5 @@ docker run --name easytier -d \
     --network host \
     -e TZ=Asia/Shanghai \
     --privileged \
-    easytier/easytier:v2.2.0 \
-    --ipv4 10.233.233.1 --network-name ${NETWORK_NAME} --network-secret ${NETWORK_SECRET} --vpn-portal wg://0.0.0.0:11013/10.14.14.0/24
+    easytier/easytier:v2.2.2 \
+    -c /app/config.toml
