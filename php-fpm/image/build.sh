8a9d657108d956e1dbdf07be3b75788f27537213
zhanglikun <x@zahui.fan>
2025-05-25 08:45:08 +0800

    add php-fpm

---
+++ php-fpm/image/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+docker-php-ext-configure mysqli
+docker-php-ext-install mysqli
+docker-php-ext-enable mysqli
+
+# gd 模块
+apt-get update && apt-get install -y \
+		libfreetype-dev \
+		libjpeg62-turbo-dev \
+		libpng-dev \
+	&& docker-php-ext-configure gd --with-freetype --with-jpeg \
+	&& docker-php-ext-install -j$(nproc) gd
+
+# zip 模块
+apt-get install -y libzip-dev
+docker-php-ext-configure zip
+docker-php-ext-install zip
+docker-php-ext-enable zip
+
+# exif 模块
+docker-php-ext-configure exif
+docker-php-ext-install exif
+docker-php-ext-enable exif
+
+# intl 模块
+apt-get install -y libicu-dev
+docker-php-ext-configure intl
+docker-php-ext-install intl 
+docker-php-ext-enable intl
+
+# imagick 模块
+apt-get install -y libmagickwand-dev
+pecl install imagick
+docker-php-ext-enable imagick
+
+php -m
---
+++ php-fpm/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker rm -f php-test
+chown -R www-data:www-data ./app 
+docker run --rm -d --name php-test \
+	--network iuxt \
+	-v ./php.ini:/usr/local/etc/php/conf.d/php.ini \
+	-v ./extensions:/extensions \
+	-v ./app:/var/www/html \
+	-p 8080:80 \
+php-test:latest
