79a689e08b798adb63b67b82651984afd802167f
zhanglikun <x@zahui.fan>
2025-05-27 14:33:08 +0800

    优化dockerfile

--- php-fpm/start.sh
+++ php-fpm/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker rm -f php-test
-chown -R www-data:www-data ./app 
+chown -R 33:33 ./app 
 docker run --rm -d --name php-test \
 	--network iuxt \
 	-v ./php.ini:/usr/local/etc/php/conf.d/php.ini \
 	-v ./extensions:/extensions \
 	-v ./app:/var/www/html \
-	-p 8080:80 \
+	-p 80:80 \
 iuxt/php:8.1.32-apache
