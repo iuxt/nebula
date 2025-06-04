e659cad1180cc2736300799bb3135b0c69fdc0b4
iuxt <iuxt@qq.com>
2025-06-04 16:59:03 +0800

    更新alist启动脚本和配置

--- alist/run.sh
+++ alist/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -d --restart=unless-stopped --network=iuxt -v ./data:/data --name="alist" test
+docker rm -f alist
+docker run -d --restart=unless-stopped \
+    --network=iuxt \
+    -v ./data:/data \
+    --name="alist" \
+    iuxt/alist:v3.45.0
+
+../public/add_config_to_nginx.sh
