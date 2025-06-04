8de3c4667f6b32e97165097e31378aa70485032e
iuxt <iuxt@qq.com>
2025-06-04 15:38:29 +0800

    完善启动脚本

--- xtemp/run.sh
+++ xtemp/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-#
+
+set -euo pipefail
+cd $(dirname $0)
+
+../public/docker-network.sh
+
+docker rm -f xtemp
 docker run --name xtemp -d \
 	--network iuxt \
 	iuxt/xtemp:latest
+
+
+../public/add_config_to_nginx.sh
