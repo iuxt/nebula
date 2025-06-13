3411f1b208a07bfe843d21a47e02d20cbd4afc50
iuxt <iuxt@qq.com>
2025-06-13 19:02:10 +0800

    挪走了v2ray

---
+++ backup/v2ray/image/build_install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir /v2ray && cd /v2ray || exit
+
+if [ "$(uname -m)" = "x86_64" ]; then
+    echo "x86"
+    wget https://github.com/v2fly/v2ray-core/releases/download/v5.29.3/v2ray-linux-64.zip
+    unzip v2ray-linux-64.zip && rm -f v2ray-linux-64.zip
+elif [ "$(uname -m)" = "aarch64" ]; then  
+    echo "arm"
+    wget https://github.com/v2fly/v2ray-core/releases/download/v5.29.3/v2ray-linux-arm64-v8a.zip
+    unzip v2ray-linux-arm64-v8a.zip && rm -f v2ray-linux-arm64-v8a.zip
+fi
---
+++ backup/v2ray/stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+cd $(dirname $0)
+
+docker rm -f v2ray
+
+rm -f ../nginx/conf.d/$(basename "$(pwd)").conf
+../nginx/reload.sh
+
