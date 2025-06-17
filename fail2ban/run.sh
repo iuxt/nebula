3d89b2a049eb82ca0486d2116111f286f5886aaf
zhanglikun <x@zahui.fan>
2025-06-17 15:04:30 +0800

    add fail2ban

---
+++ fail2ban/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker rm -f fail2ban
+
+docker run -d \
+  --name=fail2ban \
+  --net=host \
+  --cap-add=NET_ADMIN \
+  --cap-add=NET_RAW \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Etc/UTC \
+  -e VERBOSITY=-vv \
+  -v ./config:/config \
+  -v /var/log:/var/log:ro \
+  -v /path/to/vaultwarden/log:/remotelogs/vaultwarden:ro `#optional` \
+  --restart unless-stopped \
+  lscr.io/linuxserver/fail2ban:latest
