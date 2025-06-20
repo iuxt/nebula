65ff448dfff1a1890607a22c56a82866094292f1
zhanglikun <x@zahui.fan>
2025-06-20 11:44:46 +0800

    优化检测脚本

--- fail2ban/test-fail2ban/test-http-cc.sh
+++ fail2ban/test-fail2ban/test-http-cc.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-source .env
-
 i=0
 while true
 do
   echo "第 ${i} 次尝试访问"
   i=$((i+1))
-  curl "${check_http_url}"
+  curl -s "https://guanyu.babudiu.com/xxx" -o /dev/null
 done
--- fail2ban/test-fail2ban/test-sshd.sh
+++ fail2ban/test-fail2ban/test-sshd.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-source .env
-
 i=0
 while true
 do
   echo "第 ${i} 次尝试访问"
   i=$((i+1))
-  ssh root@"${check_sshd_host}" -p 2222 -i ./id_rsa
+  ssh root@"guanyu.babudiu.com" -p 2222 -i ./id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PreferredAuthentications=publickey
 done
