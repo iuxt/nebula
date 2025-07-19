0f4630a5042a728e6b7be00709be1239a0c49c25
zhanglikun <x@zahui.fan>
2025-07-19 22:32:29 +0800

    fix env

--- docmost/run.sh
+++ docmost/run.sh
@@ -4,8 +4,8 @@ docker run -d --name docmost \
     --network iuxt \
     --restart unless-stopped \
     -v ./data:/app/data/storage \
-    -e DOCMOST_DB_URL=postgres://iuxt:vzVqG9ijCcco@postgres:5432/docmost \
-    -e DOCMOST_REDIS_URL=redis://redis:6379 \
+    -e DATABASE_URL=postgresql://iuxt:vzVqG9ijCcco@postgres:5432/docmost?schema=public \
+    -e REDIS_URL=redis://redis:6379 \
     -e APP_URL=https://doc.babudiu.com \
     -e APP_SECRET=Bs29GR1Hix7k \
     docmost/docmost:latest
