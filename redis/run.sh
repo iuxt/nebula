ef7d88c35a595b424377566e77ad96e454d848d3
iuxt <iuxt@qq.com>
2025-11-14 17:16:36 +0800

    增加密码

--- redis/run.sh
+++ redis/run.sh
@@ -4,5 +4,7 @@ docker rm -f redis
 docker run -d --name redis \
     --network iuxt \
     --restart unless-stopped \
+    -p 127.0.0.1:6379:6379 \
+    --mount type=bind,source=./redis.conf,target=/app/redis.conf,readonly \
     -v ./data:/data \
-    redis:7.2-alpine
+    redis:7.2-alpine redis-server /app/redis.conf
