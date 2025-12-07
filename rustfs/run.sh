b7f30a1a3c613fcb3734cf92f9d7142f6e23426a
iuxt <iuxt@qq.com>
2025-12-07 09:30:50 +0800

    去除端口映射

--- rustfs/run.sh
+++ rustfs/run.sh
@@ -6,8 +6,6 @@ sudo chown -R 10001:10001 data logs
 docker run -d \
   --name rustfs \
   --network iuxt \
-  -p 9000:9000 \
-  -p 9001:9001 \
   -v ./data:/data \
   -v ./logs:/logs \
   --env-file .env \
